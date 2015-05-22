62032914d61b2f660197b00980d1d2d3f3d1ccae
Christoph Lorenz <clorenz71@gmail.com>
2015-05-22 16:54:44 +0200

    First try for medium

--- src/main/java/de/christophlorenz/rmmusic/persistence/jpa2/MediumRepository.java
+++ src/main/java/de/christophlorenz/rmmusic/persistence/jpa2/MediumRepository.java
@@ -3,6 +3,7 @@ package de.christophlorenz.rmmusic.persistence.jpa2;
 import de.christophlorenz.rmmusic.model.Medium;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -20,4 +21,8 @@ public interface MediumRepository extends JpaRepository<Medium, Long> {
     List<Medium> findById(@Param("id") long id);
 
     Medium findByTypeAndCode(@Param("type") int type, @Param("code") String code);
+
+    // http://www.petrikainulainen.net/programming/spring-framework/spring-from-the-trenches-new-like-expressions-of-spring-data-jpa/
+    @Query(value = "Select distinct(label) from Medium m WHERE label like :label%", nativeQuery = true)
+    List<String> findLabelsIgnoreCaseStartingWithAsc(@Param("label") String label);
 }
