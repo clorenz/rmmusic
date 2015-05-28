593c92d08d5b01f5df9a0ce9d5cef32db45a4511
Christoph Lorenz <clorenz71@gmail.com>
2015-05-28 14:04:32 +0200

    Added LPs

---
+++ src/main/java/de/christophlorenz/rmmusic/web/controller/LPController.java
@@ -0,0 +1,43 @@
+package de.christophlorenz.rmmusic.web.controller;
+
+import de.christophlorenz.rmmusic.model.Medium;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by clorenz on 28.05.15.
+ */
+@Controller
+@RequestMapping("/rmmusic/medium/lp")
+public class LPController extends MediumController {
+
+    @Override
+    protected String getSelectFormTemplate() {
+        return "rmmusic/selectLPForm";
+    }
+
+    @Override
+    protected String getEditFormTemplate() {
+        return "rmmusic/editLPForm";
+    }
+
+    @Override
+    protected String getListFormTemplate() {
+        return "rmmusic/mediaList";
+    }
+
+    @Override
+    protected String getMediumTypeName() {
+        return "LP";
+    }
+
+    @Override
+    protected String getMediumTypeAbbreviation() {
+        return "L";
+    }
+
+    @Override
+    protected int getMediumType() {
+        return Medium.LP;
+    }
+}
